65d1222ea70c93247b07cd39f3eba5a8c32fd66e
William Panting <willtp87@hotmail.com>
2011-05-05 14:08:26 -0300

    Enhanced Readme, added pdf append skeleton

--- README.txt
+++ README.txt
@@ -1,3 +1,6 @@
 @author: William Panting
 
-@dependencies : Kakadu, ABBYY CLI, ImageMagick, Python 2.7, Lame, FFmpeg, SWFTools
+@dependencies: Kakadu, ABBYY CLI, ImageMagick, Python 2.7, Lame, FFmpeg, SWFTools, pyPDF, lxml
+
+This is a general python library module that is meant to assist in Islandora development.
+It contains document manipulations, conversions, OCR using ABBYY, fedora helper code and xml helper code.
---
+++ islandoraUtils/fileManipulator.py
@@ -0,0 +1,14 @@
+'''
+Created on May 5, 2011
+
+@author: William Panting
+This file is meant to help with file manipulations/alterations.
+'''
+from pyPdf import PdfFileWriter, PdfFileReader
+'''
+@param origionalFile: a string representing the path of the file that is to be modified
+@param fileToAppend: a string representing the path of the file that is to be appended to the orgional file
+@return bool: true if successful false if not
+'''
+def appendPDFwithPDF(origionalFile,fileToAppend):
+    return True
