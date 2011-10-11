554ed5b38dc19fb7caba15da846aa9355a443c46
Jonathan Green <jonathan@discoverygarden.ca>
2011-10-11 08:51:29 -0600

    Updated the DSConverter to use its own logger

--- islandoraUtils/DSConverter.py
+++ islandoraUtils/DSConverter.py
@@ -29,33 +29,9 @@ from lxml import etree
 # thumbnail constants
 tn_postfix = '-tn.jpg'
 tn_size = (150, 200)
-'''
-#handle constants
-handleServer='damocles.coalliance.org'
-handleServerPort='9080'
-handleServerApp='/handles/handle.jsp?'
 
-
-def get_handle(obj):
-    try:
-      conn = httplib.HTTPConnection(handleServer,handleServerPort,timeout=10)
-      conn.request('GET', handleServerApp+'debug=true&adr3=true&pid='+obj.pid)
-      res = conn.getresponse()
-    except:
-      logging.error("Error connecting to Handle Server. PID: %s." % (obj.pid))
-      return False
-
-    # convert the response to lowercase and see if it contains success
-    text = string.lower(res.read())
-
-    if ( string.find(text,'==>success') != -1 ):
-        logging.info("Successfuly created handle for %s." % obj.pid)
-        return True
-    else:
-        logging.info("Failed to create handle for %s." % obj.pid)
-        return False
-'''
 def create_thumbnail(obj, dsid, tnid):
+    logger = logging.getLogger('islandoraUtils.DSConverter.create_thumbnail')
     # We receive a file and create a jpg thumbnail
     directory, file = get_datastream_as_file(obj, dsid, "tmp")
     
@@ -66,31 +42,32 @@ def create_thumbnail(obj, dsid, tnid):
     if r == 0:
         update_datastream(obj, tnid, directory+'/'+tnid, label='thumbnail', mimeType='image/jpeg')
     else :
-        logging.warning('PID:%s DSID:%s Thumbnail creation failed (return code:%d).' % (obj.pid, dsid, r))
+        logger.warning('PID:%s DSID:%s Thumbnail creation failed (return code:%d).' % (obj.pid, dsid, r))
         #Try again on failure?
        
-    logging.debug(directory)
-    logging.debug(file)
-    logging.debug(tnid)
-    logging.debug(os.listdir(directory))
+    logger.debug(directory)
+    logger.debug(file)
+    logger.debug(tnid)
+    logger.debug(os.listdir(directory))
 
     rmtree(directory, ignore_errors=True)
     return r
 
 def create_jp2(obj, dsid, jp2id):
+    logger = logging.getLogger('islandoraUtils.DSConverter.create_jp2')
     # We receive a TIFF and create a Lossless JPEG 2000 file from it.
     directory, file = get_datastream_as_file(obj, dsid, 'tiff') 
     r = subprocess.call(["convert", directory+'/'+file, '+compress', '-colorspace', 'RGB', directory+'/uncompressed.tiff'])
     if r != 0:
-        logging.warning('PID:%s DSID:%s JP2 creation failed (convert return code:%d).' % (obj.pid, dsid, r))
+        logger.warning('PID:%s DSID:%s JP2 creation failed (convert return code:%d).' % (obj.pid, dsid, r))
         rmtree(directory, ignore_errors=True)
         return r;
     r = subprocess.call(["kdu_compress", "-i", directory+'/uncompressed.tiff', "-o", directory+"/tmpfile_lossy.jp2", "-rate", "0.5", "Clayers=1", "Clevels=7", "Cprecincts={256,256},{256,256},{256,256},{128,128},{128,128},{64,64},{64,64},{32,32},{16,16}", "Corder=RPCL", "ORGgen_plt=yes", "ORGtparts=R", "Cblk={32,32}", "Cuse_sop=yes"])
     if r != 0:
-        logging.warning('PID:%s DSID:%s JP2 creation failed. Trying alternative.' % (obj.pid, dsid))
+        logger.warning('PID:%s DSID:%s JP2 creation failed. Trying alternative.' % (obj.pid, dsid))
     	r = subprocess.call(["convert", directory+'/'+file, '-compress', 'JPEG2000', '-quality', '50%', directory+'/tmpfile_lossy.jp2'])
         if r != 0:
-            logging.warning('PID:%s DSID:%s JP2 creation failed (kdu_compress return code:%d).' % (obj.pid, dsid, r))
+            logger.warning('PID:%s DSID:%s JP2 creation failed (kdu_compress return code:%d).' % (obj.pid, dsid, r))
 
     if r == 0:
         update_datastream(obj, jp2id, directory+'/tmpfile_lossy.jp2', label='Compressed JPEG2000', mimeType='image/jp2')
@@ -99,6 +76,7 @@ def create_jp2(obj, dsid, jp2id):
     return r
 
 def create_mp3(obj, dsid, mp3id):
+    logger = logging.getLogger('islandoraUtils.DSConverter.create_mp3')
     # We recieve a WAV file. Create a MP3
     directory, file = get_datastream_as_file(obj, dsid, "wav")
     
@@ -108,12 +86,13 @@ def create_mp3(obj, dsid, mp3id):
     if r == 0:
       update_datastream(obj, mp3id, directory+'/'+mp3id, label='compressed to mp3', mimeType='audio/mpeg')
     else:
-      logging.warning('PID:%s DSID:%s MP3 creation failed (lame return code:%d).' % (obj.pid, dsid, r))
+      logger.warning('PID:%s DSID:%s MP3 creation failed (lame return code:%d).' % (obj.pid, dsid, r))
 
     rmtree(directory, ignore_errors=True)
     return r
 
 def create_ogg(obj, dsid, oggid):
+    logger = logging.getLogger('islandoraUtils.DSConverter.create_ogg')
     #recieve a wav file create a OGG
     directory, file = get_datastream_as_file(obj, dsid, "wav")
     
@@ -122,22 +101,23 @@ def create_ogg(obj, dsid, oggid):
     if r == 0:
         update_datastream(obj, oggid, directory+'/'+oggid, label='compressed to ogg', mimeType='audio/ogg')
     else:
-        logging.warning('PID:%s DSID:%s OGG creation failed (ffmpeg return code:%d).' % (obj.pid, dsid, r))
+        logger.warning('PID:%s DSID:%s OGG creation failed (ffmpeg return code:%d).' % (obj.pid, dsid, r))
     rmtree(directory, ignore_errors=True)
     return r
 
 def create_swf(obj, dsid, swfid):
+    logger = logging.getLogger('islandoraUtils.DSConverter.create_swf')
     #recieve PDF create a SWF for use with flexpaper
     directory, file = get_datastream_as_file(obj, dsid, "pdf")
     
     r = subprocess.call(['pdf2swf', directory+'/'+file, '-o', directory+'/'+swfid,\
          '-T 9', '-f', '-t', '-s', 'storeallcharacters', '-G'])
     if r != 0:
-        logging.warning('PID:%s DSID:%s SWF creation failed. Trying alternative.' % (obj.pid, dsid))
+        logger.warning('PID:%s DSID:%s SWF creation failed. Trying alternative.' % (obj.pid, dsid))
         r = subprocess.call(['pdf2swf', directory+'/'+file, '-o', directory+'/'+swfid,\
              '-T 9', '-f', '-t', '-s', 'storeallcharacters', '-G', '-s', 'poly2bitmap'])
         if r != 0:
-            logging.warning('PID:%s DSID:%s SWF creation failed (pdf2swf return code:%d).' % (obj.pid, dsid, r))
+            logger.warning('PID:%s DSID:%s SWF creation failed (pdf2swf return code:%d).' % (obj.pid, dsid, r))
 
     if r == 0:
         update_datastream(obj, swfid, directory+'/'+swfid, label='pdf to swf', mimeType='application/x-shockwave-flash')
@@ -146,6 +126,7 @@ def create_swf(obj, dsid, swfid):
     return r
 
 def create_pdf(obj, dsid, pdfid):
+    logger = logging.getLogger('islandoraUtils.DSConverter.create_pdf')
     #recieve document and create a PDF with libreoffice if possible
     directory, file = get_datastream_as_file(obj, dsid, "document")
     
@@ -159,7 +140,7 @@ def create_pdf(obj, dsid, pdfid):
         value = 0
     else:
         value = 1
-        logging.warning('PID:%s DSID:%s PDF creation failed.' % (obj.pid, dsid))
+        logger.warning('PID:%s DSID:%s PDF creation failed.' % (obj.pid, dsid))
 
     rmtree(directory, ignore_errors=True)
     return value
