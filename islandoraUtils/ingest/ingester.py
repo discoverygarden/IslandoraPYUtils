c568bf0280aa4b99c92f29af146f68e713e1c2e9
William Panting <willtp87@hotmail.com>
2012-05-10 14:51:30 -0300

    added icon for collections

--- islandoraUtils/ingest/ingester.py
+++ islandoraUtils/ingest/ingester.py
@@ -22,6 +22,7 @@ class ingester(object):
     '''
     This is the kingpin.  This object should handle creating all the other basic ingest helpers.
     @TODO: add namespace and fcrepo properties
+    @todo: add an add default TN method
     @todo: implement (this will mean a user will not need to know the content_model for a collection, or the pid of top
             both of wich could come from the config file)
                 def ingest_collection_object(ingester, parent_pid=None):
