01302b7b0f742162b7a2e42cae0b8350f9cb57be
William Panting <willtp87@hotmail.com>
2012-08-27 12:16:09 -0300

    build is not failing anymore

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ Created on Apr 18, 2011
 Setup script for isladoraUtils
 
 @author: William Panting
+@todo:
+    move xlsts into their own subdirectory of __resources
 '''
 import os
 from setuptools import setup, find_packages
@@ -13,15 +15,17 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-      name='islandoraUtils',
-      version='0.1',
-      description='A Package meant to assist Islandora developers',
-      author='William Panting',
-      author_email='will@discoverygarden.ca',
+      name = 'islandoraUtils',
+      version = '0.1',
+      description = 'A Package meant to assist Islandora developers',
+      author = 'William Panting',
+      author_email = 'will@discoverygarden.ca',
       license = "GPL",
-      packages=find_packages(),
-      package_dir={"islandoraUtils" : "islandoraUtils"},
-      package_data={"islandoraUtils" : ["__resources/*"]},
-      long_description=read('README'),
-      install_requires=['setuptools','lxml']
+      packages = find_packages(),
+      package_dir = {"islandoraUtils" : "islandoraUtils"},
+      package_data = {"islandoraUtils" : ["__resources/*.xslt",
+                                          "__resources/SPARQL/*",
+                                          "__resources/images/icons/*",]},
+      long_description = read('README'),
+      install_requires = ['setuptools','lxml']
       )
